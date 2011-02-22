d79180565bdd6f533653ec22fd27f14597566f79
matthew jording <mjording@opengotham.com>
2011-02-22 03:16:16 -0500

    get a tweet test

--- spec/birdFlippr_spec.rb
+++ spec/birdFlippr_spec.rb
@@ -1,11 +1,15 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe "Birdflippr" do
-  it "should be able to display tweets" do
-    @client = BirdFlipper.new(ENV['TWITTERCK'],ENV['TWITTERCS'])
-    @client.get_tweets.should_not be_empty
+	#This gets run once and only once in its own instance of Object
+	before(:all) do
+		@client = BirdFlipper.new(ENV['TWITTERCK'],ENV['TWITTERCS'])
+	end
+  it "should be able to get tweets" do
+      @client.get_tweets.should_not be_empty
   end
-
-
+	it "should be able to get the a questionable by its status id" do
+		 tweet = @client.get_status(39807438735671297)
+		 tweet.text.should match(/making an @dhh salad/)
+	end
 end
-
